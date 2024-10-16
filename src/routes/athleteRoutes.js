6a1d9220470162b8615ab076dd1bc72ce55963cd
Alexandre Queiroz <xandeq@gmail.com>
2024-10-16 20:03:21 -0300

    Changes

--- src/routes/athleteRoutes.js
+++ src/routes/athleteRoutes.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const {
   createAthlete,
   getAthleteProfile,
   listAthletes,
   updateAthlete,
-  deleteAthlete
-} = require('../controllers/athleteController');  // Certifique-se de que os controladores estão sendo importados corretamente
+  deleteAthlete,
+} = require("../controllers/athleteController");
 
 const router = express.Router();
 
 // Definição das rotas
-router.post('/create', createAthlete); // Rota para criar um novo atleta
-router.get('/:atletaId', getAthleteProfile); // Rota para visualizar o perfil de um atleta
-router.get('/list', listAthletes); // Rota para listar todos os atletas
-router.put('/update/:id', updateAthlete); // Rota para atualizar os dados de um atleta específico
-router.delete('/delete/:id', deleteAthlete); // Rota para deletar um atleta específico
+router.post("/create", createAthlete); // Rota para criar um novo atleta
+router.get("/:atletaId", getAthleteProfile); // Rota para visualizar o perfil de um atleta
+router.get("/list", listAthletes); // Rota para listar todos os atletas
+router.put("/update/:id", updateAthlete); // Rota para atualizar os dados de um atleta específico
+router.delete("/delete/:id", deleteAthlete); // Rota para deletar um atleta específico
 
 module.exports = router;
