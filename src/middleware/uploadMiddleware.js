9b20bc55d2d9cee52c116536d2e7724a44ed98f5
Alexandre Queiroz <xandeq@gmail.com>
2024-10-22 20:03:17 -0300

    Teste

--- src/middleware/uploadMiddleware.js
+++ src/middleware/uploadMiddleware.js
@@ -3,21 +3,21 @@ const multerS3 = require("multer-s3");
 const AWS = require("aws-sdk");
 require("dotenv").config();
 
-// Configurar o S3
+// Configurar AWS SDK
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
 
-// Configurar o multer para usar o S3 como armazenamento
+// Middleware de upload usando multer e multer-s3
 const upload = multer({
   storage: multerS3({
     s3,
-    bucket: process.env.S3_BUCKET_NAME,
-    acl: "public-read", // Permissão de leitura pública para os arquivos
-    key: (req, file, cb) => {
-      cb(null, `midias/${Date.now().toString()}_${file.originalname}`);
+    bucket: process.env.S3_BUCKET_NAME, // Certifique-se de que o bucket esteja definido no .env
+    acl: "public-read", // Permissões de leitura pública
+    key: function (req, file, cb) {
+      cb(null, `uploads/${Date.now().toString()}_${file.originalname}`);
     },
   }),
 });
