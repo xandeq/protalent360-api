82ad20ff00fa2d62572c3ca31e70d83a5a6ea6d5
Alexandre Queiroz <xandeq@gmail.com>
2024-10-24 07:53:26 -0300

    Pool

--- src/controllers/escolinhasController.js
+++ src/controllers/escolinhasController.js
@@ -1,4 +1,4 @@
-const connection = require("../config/db"); // Importa conexão com banco
+const connection = require("../config/db"); // Importa conexão com banco já com Promises
 
 // Função para criar escolinha
 exports.cadastrarEscolinha = async (req, res) => {
@@ -20,7 +20,8 @@ exports.cadastrarEscolinha = async (req, res) => {
             INSERT INTO escolinhas (nome, cidade, estado, pais, categorias, redes_sociais, descricao, infraestrutura, emblemas_ranking)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
-    await connection.pool.query(query, [
+    // Executa a query de inserção usando await
+    await connection.query(query, [
       nome,
       cidade,
       estado,
@@ -39,15 +40,14 @@ exports.cadastrarEscolinha = async (req, res) => {
   }
 };
 
-exports.listarEscolinhas = (req, res) => {
-  const query = "SELECT * FROM escolinhas";
-
-  connection.pool.query(query, (error, results) => {
-    if (error) {
-      return res
-        .status(500)
-        .json({ message: "Erro ao listar escolinhas", error });
-    }
+// Função para listar escolinhas
+exports.listarEscolinhas = async (req, res) => {
+  try {
+    const query = "SELECT * FROM escolinhas";
+    const [results] = await connection.query(query); // Executa a query de listagem
     res.status(200).json(results);
-  });
+  } catch (error) {
+    console.error("Erro ao listar escolinhas:", error);
+    res.status(500).json({ message: "Erro ao listar escolinhas", error });
+  }
 };
