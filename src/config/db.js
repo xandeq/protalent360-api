dfa494c2b88cac0f0d27e3a8921d821dcfd95188
Alexandre Queiroz <xandeq@gmail.com>
2024-11-03 19:19:48 -0300

    pool

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
-import { createPool } from "mysql2";
+// db.js - Configuração do Banco de Dados MySQL usando Pool de Conexões
+import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
 // Criar o pool de conexões com o MySQL usando as variáveis do .env
-const pool = createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -11,7 +12,7 @@ const pool = createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-}).promise(); // Use .promise() aqui para ativar Promises
+}).promise();
 
-// Exportar o pool
-export default { pool };
+// Exportar o pool como padrão
+export default pool;
