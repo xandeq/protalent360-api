7613024db08d710f9116faa073d2966fc7d501ef
Alexandre Queiroz <xandeq@gmail.com>
2024-11-04 18:40:05 -0300

    upload

--- src/routes/midiasRoutes.js
+++ src/routes/midiasRoutes.js
@@ -1,10 +1,8 @@
 import express from "express";
-import { upload } from "../middleware/uploadMiddleware.js";
-import { getPresignedUrl, saveMidiaRecord, uploadMidia } from "../controllers/midiasController.js";
+import { getPresignedUrl, saveMidiaRecord } from "../controllers/midiasController.js";
 
 const router = express.Router();
 
-router.post("/upload", upload.single("midia"), uploadMidia);
 router.post("/get-presigned-url", getPresignedUrl);
 router.post("/save-midia-record", saveMidiaRecord);
 
